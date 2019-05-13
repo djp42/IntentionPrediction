bd4bc144d70d930cdc24839be7eef3e6480d0477
D <djp42@stanford.edu>
2019-05-13 13:32:53 -0700

    fix sample unittest

--- test/test_imports.py
+++ test/test_imports.py
@@ -2,5 +2,8 @@ import unittest
 
 class SampleTestCase(unittest.TestCase):
     def test_python_runs(self):
-        for _ in range(100):
+        for _ in range(10000):
             self.assertEqual(2+2, 4)
+
+if __name__ == '__main__':
+    unittest.main()
